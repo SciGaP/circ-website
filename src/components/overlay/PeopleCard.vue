98a0228e567568a31434d3e5eaf8862a63b23d76
Krishna Sannidhi <sannidhikrishna212@gmail.com>
2021-08-17 23:47:59 -0700

    Added teams page.

---
+++ src/components/overlay/PeopleCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="link-text" style="width:552px">
+    <div class="f-s pos-r">
+      <div>
+        <img width="215px" height="197px" :src="require(`@/assets/${imageLink}`)" :alt="firstName[0].toUpperCase()+''+lastName[0].toUpperCase()">
+      </div>
+      <div>
+        <span class="crim-colo" style="font-size:32px;line-height:140%;font-weight:600;">{{ firstName }} <span style="color:#000000"></span></span><br/>
+        <span style="font-size:20px;line-height:140%;font-weight:600;padding-top:9px">{{ position }}</span>
+        <a href="" style="padding-top:9px">View Google scholar Profile ></a><br/>
+        <span style="font-size:20px;line-height:160%;color:#333333;padding-top:5px">ORCID: <u>{{ orc_id }}</u></span>
+      </div>
+    </div>
+    <p style="font-size:20px;line-height:30px;color:#000000">{{ description }}</p>
+    <div class="f-s pos-r">
+      <div>
+        <span style="font-size:20px;line-height:24px">Talk to Marcus about :</span>
+      </div>
+      <div class="grid-container--2">
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PeopleCard',
+  peops: {
+    firstName: String, 
+    lastName: String, 
+    profileLink: String, 
+    imageLink: String,
+    description: String, 
+    position: String, 
+    orc_id: String, 
+    role: String,
+    talkAbout: Array
+  }
+}
+</script>
+
+<style scoped>
+
+img {
+  border-radius: 50%;
+}
+
+a {
+  text-decoration: none;
+  color: #006298;
+}
+
+</style>
--- src/components/pages/Teams.vue
+++ src/components/pages/Teams.vue
@@ -1,6 +1,21 @@
 <template>
-	<div>
-			
+	<div class=link-text>
+		<div class="">
+			<img src="@/assets/Teams_logo.png" alt="">
+		</div>
+
+		<div class="global-padding">
+			<div style="padding-bottom:2rem"><span class="home-head-text head-text link-text">Our Team is our strength</span></div>
+			<p style="font-size:20px;ine-height:34px">The Caliber of our team is defined by its multidisciplinary nature. We are a group of professionals ranging from Computer Scientists to Domain Experts. The breadth of expertise our team covers lets us customize our solution to your needs.</p>
+
+			<div style="padding-bottom:2rem"><span class="home-head-text head-text link-text">People</span></div>
+
+		</div>
+
+		<div class="global-padding" style="background: rgba(234, 234, 234, 0.4);">
+			<span style="font-size:44px;line-height:120%"><b>Have a <span class="crim-colo">project</span> in mind? <br/>
+			Our <span class="crim-colo">team</span> can <span class="crim-colo">help</span> you.</b></span>
+		</div>
 	</div>
 </template>
 
