98f5f2c4891baf4e95456581beea185044a8faf1
Hareesh Bondalapati <hai.hareeshb@gmail.com>
2021-08-19 15:49:37 -0400

    added resuable publicationcard component

---
+++ src/components/overlay/publicationCard.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="publication-card link-text">
+		<div style="padding:0px 15px">
+			<span style="font-size:24px;line-height:120%;color:#333333"><b>{{ name }}</b></span><br/><br/>
+			<span style="padding-top:15px;font-size:20px;font-style:italic;ine-height:140%;color:#990000;align:center">{{ authors }}</span>
+			<p style="font-size:20px;line-height:160%;color:#45382B">{{ description }}</p>
+			<a href="" style="font-size:20px line-height:140%">View Publication ></a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'publicationCard',
+	props: ['name', 'authors', 'description', 'publicationLink']
+}
+</script>
+
+<style scoped>
+
+.publication-card {
+	background: #FFFFFF;
+	box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.05);
+	border-radius: 5px;
+    padding: 10px 60px;
+}
+
+img {
+	width: 387px;
+	height: 191px;
+}
+
+a {
+  text-decoration: none;
+  color: #006298;
+}
+</style>
--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -81,22 +81,97 @@
 
         <div class="global-padding" style="background:#FAF9F9">
 			<div style="padding-bottom:2rem"><span class="home-head-text head-text link-text">Key Publications</span></div>
+            <div>
+                <ol>
+                    <li>
+                            <publicationCard 
+                                name="Apache Airavata as a laboratory: architecture and case study for component-based gateway middleware."
+                                authors="Marru Suresh, Marlon Pierce, Sudhakar Pamidighantam, and Chathuri Wimalasena"
+                                description="In Proceedings of the 1st Workshop on The Science of Cyberinfrastructure: Research, Experience, Applications and Models, pp. 19-26. ACM, 2015."
+                            />
+                    </li>
+                    <br/><br/>
+                    <li>
+                            <publicationCard 
+                                name="Apache Airavata as a laboratory: architecture and case study for component-based gateway middleware."
+                                authors="Marru Suresh, Marlon Pierce, Sudhakar Pamidighantam, and Chathuri Wimalasena"
+                                description="In Proceedings of the 1st Workshop on The Science of Cyberinfrastructure: Research, Experience, Applications and Models, pp. 19-26. ACM, 2015."
+                            />
+                    </li>
+                    <br/>
+                    <br/>
+                    <li>
+                            <publicationCard 
+                                name="Apache Airavata as a laboratory: architecture and case study for component-based gateway middleware."
+                                authors="Marru Suresh, Marlon Pierce, Sudhakar Pamidighantam, and Chathuri Wimalasena"
+                                description="In Proceedings of the 1st Workshop on The Science of Cyberinfrastructure: Research, Experience, Applications and Models, pp. 19-26. ACM, 2015."
+                            />
+                    </li>
+                    <br/>
+                    <br/>
+                    <li>
+                            <publicationCard 
+                                name="Apache Airavata as a laboratory: architecture and case study for component-based gateway middleware."
+                                authors="Marru Suresh, Marlon Pierce, Sudhakar Pamidighantam, and Chathuri Wimalasena"
+                                description="In Proceedings of the 1st Workshop on The Science of Cyberinfrastructure: Research, Experience, Applications and Models, pp. 19-26. ACM, 2015."
+                            />
+                    </li><br/>
+                    <br/>
+
+                </ol>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import projectCard from '../overlay/projectCard';
+import publicationCard from '../overlay/publicationCard';
 export default {
 	name: 'projects',
-	components: {projectCard},
-
+	components: {
+        projectCard,
+        publicationCard
+        }
 }
 </script>
 <style scoped>
 
 img {
 	filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));border-radius: 5px 50px 5px 50px;
+}
+ol{
+    counter-reset: li;
+}
+ol > li {
+    position:relative; /* Create a positioning context */
+    margin:0 0 0 2em; /* Give each list item a left margin to make room for the numbers */
+    padding:0px 0px; /* Add some spacing around the content */
+    list-style:none; /* Disable the normal item numbering */
+    background:#f6f6f6;
+    counter-increment:li; 
+}
+ol > li:before {
+    
+    content:counter(li); 
+    position:absolute;
+    top:0px;
+    left:0em;
+    -moz-box-sizing:border-box;
+    -webkit-box-sizing:border-box;
+    box-sizing:border-box;
+    width:61px;
+    height:66px;
+    margin-right:8px;
+    padding:4px;
+    border-top:2px;
+    color:#FFFFFF;
+    background:#990000;
+    font-weight:bold;
+    font-family:Source Sans Pro;
+    font-size:40px;
+    line-height:50px;
+    text-align:center;
 }
 
 </style>
